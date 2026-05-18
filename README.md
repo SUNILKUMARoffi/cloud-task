f5dff11aa0ec6c81b82475d5bc55570c789b8b63
Sunil <nsunilkumarcse@gmail.com>
2026-05-18 15:52:45 +0530

    Add README file

---
+++ README.md
@@ -0,0 +1,18 @@
+\# Cloud Task Manager
+
+
+
+Backend API built using FastAPI.
+
+
+
+\## Features
+
+\- CRUD APIs
+
+\- FastAPI backend
+
+\- Swagger UI
+
+\- Git version control
+
--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
-app=FastAPI()
+app = FastAPI()
+
+tasks = []
+
+
+class Task(BaseModel):
+    id: int
+    title: str
+
 
 @app.get("/")
 def home():
-	return{"message":"Task API running"}
+    return {"message": "Task API Running"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task.dict())
+    return {
+        "message": "Task created successfully",
+        "task": task
+    }
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return {"message": "Task deleted successfully"}
+
+    return {"error": "Task not found"}
